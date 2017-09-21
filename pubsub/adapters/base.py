fb8e581f0f60de4b606a760f28f25ed000063a76
William Stewart <zoidbergwill@gmail.com>
2017-09-21 14:15:50 +0200

    This __init__ does nothing

--- pubsub/adapters/base.py
+++ pubsub/adapters/base.py
@@ -3,9 +3,6 @@ class BaseAdapter(object):
     PubSub adapter base class
     """
 
-    def __init__(self):
-        pass
-
     def publish(self, channel, message):
         raise NotImplementedError('Not implemented')
 
