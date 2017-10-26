8cb9dcbbd951b3197bd0420b1b0771d7f824bdd2
William Stewart <zoidbergwill@gmail.com>
2017-10-26 16:30:46 +0200

    Debug logs are poopy

--- pubsub/protocol.py
+++ pubsub/protocol.py
@@ -44,8 +44,6 @@ class Protocol:
                     now = datetime.utcnow()
                     global time_since_last
                     time_since_last = now - last_message
-                    if time_since_last.seconds:
-                        logging.info('{} seconds since last message'.format(time_since_last))
                     last_message = now
 
             try:
