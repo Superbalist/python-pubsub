9b2c5b84bd0eecab24d5755af229bca3c4da4f0f
William Stewart <zoidbergwill@gmail.com>
2017-11-15 10:20:49 +0200

    Don't limit to 10 messages unacked, default is 1000

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='pubsub.py',
     author='Superbalist Engineering',
     author_email='tech@superbalist.com',
-    version='0.3.1',
+    version='0.3.2',
     description='Python PubSub Adapter for gcloud',
     url='https://github.com/Superbalist/python-pubsub',
     install_requires=[
