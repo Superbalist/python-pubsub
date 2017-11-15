17b3eedead1a3d6f45a8b809d5c978843b57d035
William Stewart <zoidbergwill@gmail.com>
2017-11-15 10:09:09 +0200

    Bump to 0.3.1 to actually return subscription future
    We need a changelog

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='pubsub.py',
     author='Superbalist Engineering',
     author_email='tech@superbalist.com',
-    version='0.3.0',
+    version='0.3.1',
     description='Python PubSub Adapter for gcloud',
     url='https://github.com/Superbalist/python-pubsub',
     install_requires=[
