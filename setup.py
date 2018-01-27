15ccfde1ec13f0fcf3ed49c4ffcd5a514a4015f0
William Stewart <zoidbergwill@gmail.com>
2018-01-27 09:17:14 +0200

    Bump to version 0.3.4
    Added validation_error_callback and create_topic to adapter.subscriber

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='pubsub.py',
     author='Superbalist Engineering',
     author_email='tech@superbalist.com',
-    version='0.3.3',
+    version='0.3.4',
     description='Python PubSub Adapter for gcloud',
     url='https://github.com/Superbalist/python-pubsub',
     install_requires=[
