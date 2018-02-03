55a69ec1e05f3fb34f9fd9b47ee67cbb7034e4d6
William Stewart <zoidbergwill@gmail.com>
2018-02-03 10:31:09 +0200

    Bump version to 0.3.5
    to release HTTP adapter

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='pubsub.py',
     author='Superbalist Engineering',
     author_email='tech@superbalist.com',
-    version='0.3.4',
+    version='0.3.5',
     description='Python PubSub Adapter for gcloud',
     url='https://github.com/Superbalist/python-pubsub',
     install_requires=[
