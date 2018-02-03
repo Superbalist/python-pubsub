bf9c3a6c15cc289291d488f47a8662032f3307eb
William Stewart <zoidbergwill@gmail.com>
2018-02-03 11:56:57 +0200

    Deprecate single publish to pubsub-rest-proxy cuz it's gross with how we do serialisation

--- pubsub/adapters/http.py
+++ pubsub/adapters/http.py
@@ -9,14 +9,6 @@ class HttpAdapter(object):
         self.session = Session()
         self.base_url = base_url or 'http://127.0.0.1:3000'
 
-    def publish(self, channel, message):
-        # We could use json=, but the message is already dumped and encoded
-        response = self.session.post(
-            '{}/messages/{}'.format(self.base_url, channel),
-            data=b'{"messages": [%b]}' % message, headers={'Content-Type': 'application/json'})
-        response.raise_for_status()
-        return response.json()
-
     def bulk_publish(self, channel, messages):
         # We could use json=, but the message is already dumped and encoded
         response = self.session.post(
