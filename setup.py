61e54601f75cbc2b0d01ed2430a8d3c96330fa70
Bertus Steenberg <bertus.s@superbalist.com>
2018-11-21 15:20:36 +0200

    do less

--- setup.py
+++ setup.py
@@ -1,11 +1,5 @@
 from setuptools import setup, find_packages
 
-# read the contents of your README file
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
 
 setup(
     name='pubsub.py',
@@ -13,8 +7,7 @@ setup(
     author_email='tech@superbalist.com',
     version='0.3.10',
     description='Python PubSub Adapter for gcloud',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description='Python PubSub Adapter for gcloud',
     url='https://github.com/Superbalist/python-pubsub',
     install_requires=[
         'google.cloud.pubsub==0.33.1',
