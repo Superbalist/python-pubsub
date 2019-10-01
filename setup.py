c2956eb7cc8c79c534ebf0b712aa365f97d01f5a
Bertus Steenberg <bertus.s@superbalist.com>
2019-10-01 16:26:31 +0200

    tox

--- setup.py
+++ setup.py
@@ -2,17 +2,14 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='pubsub.py',
-    author='Superbalist Engineering',
-    author_email='tech@superbalist.com',
-    version='1.0.0',
-    description='Python PubSub Adapter for gcloud',
-    long_description='Python PubSub Adapter for gcloud',
-    url='https://github.com/Superbalist/python-pubsub',
-    install_requires=[
-        'google.cloud.pubsub==0.33.1',
-        'jsonschema',
-        'requests',
-    ],
+    name="pubsub.py",
+    author="Superbalist Engineering",
+    author_email="tech@superbalist.com",
+    version="1.0.0",
+    description="Python PubSub Adapter for gcloud",
+    long_description="Python PubSub Adapter for gcloud",
+    url="https://github.com/Superbalist/python-pubsub",
+    install_requires=["google-cloud-pubsub~=1.0.2", "jsonschema", "requests"],
     packages=find_packages(),
-    zip_safe=False)
+    zip_safe=False,
+)
