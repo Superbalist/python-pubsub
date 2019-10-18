f9961ca5a9a14a246d364bdc0854c5a747f9665b
Bertus Steenberg <bertus.s@superbalist.com>
2019-10-18 15:46:09 +0200

    Increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="pubsub.py",
     author="Superbalist Engineering",
     author_email="tech@superbalist.com",
-    version="1.1.0-beta",
+    version="1.1.0-beta2",
     description="Python PubSub Adapter for gcloud",
     long_description="Python PubSub Adapter for gcloud",
     url="https://github.com/Superbalist/python-pubsub",
